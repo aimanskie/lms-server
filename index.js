f91ecc44272da5ce967d99325ec3750c17491668
aimanskie <aimanskie@gmail.com>
2022-12-08 16:16:13 +0800

    cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { readdirSync } from 'fs'
 import mongoose from 'mongoose'
 import csrf from 'csurf'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const morgan = require('morgan')
 const app = express()
 const port = process.env.PORT || 8000
@@ -19,6 +20,7 @@ mongoose
   .then(() => app.listen(port, () => console.log(`Server is running on port ${port}`)))
   .catch((err) => console.log('db connection error => ', err))
 
+app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
 app.use(morgan('dev'))
