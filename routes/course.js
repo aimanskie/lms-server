1a451ab2902e12baeb781d465043ae5866d60ef0
aimanskie <aiman@assohwah.com>
2022-12-13 17:39:52 +0800

    updated

--- routes/course.js
+++ routes/course.js
@@ -34,7 +34,12 @@ router.post('/course/remove-image', removeImage)
 router.post('/course', requireSignin, isInstructor, create)
 router.put('/course/:slug', requireSignin, update)
 router.get('/course/:slug', read)
-router.post('/course/video-upload/:instructorId', requireSignin, formidable(), uploadVideo)
+router.post(
+  '/course/video-upload/:instructorId',
+  requireSignin,
+  formidable({ maxFileSize: 500 * 1024 * 1024 }),
+  uploadVideo
+)
 router.post('/course/video-remove/:instructorId', requireSignin, removeVideo)
 
 router.put('/course/publish/:courseId', requireSignin, publishCourse)
